6b2cd1d580e99c5358e46123ce7b6325702e7278
luistf24 <luis.tormo24@gmail.com>
2021-01-16 22:56:53 +0100

    Modifico fallo en la conexión

--- sub2.py
+++ sub2.py
@@ -1,28 +1,37 @@
 import cx_Oracle
 import datetime
 
-def addHistorial(cursor,dni,telefono,pya,gs,dniempleado):
+def addHistorial(conexion,dni,telefono,pya,gs,dniempleado):
+    cursor = conexion.cursor()
+
     try:
         cursor.callproc('Historiales.addHistorial',[dni,telefono,pya,gs,dniempleado])
     except cx_Oracle.IntegrityError as error:
         print('...Error añadiendo el nuevo historial:\n\t{}\n'.format(error))
 
 
-def modHistorial(cursor,dni,telefono,pya,gs,dniempleado):
+def modHistorial(conexion,dni,telefono,pya,gs,dniempleado):
+    cursor = conexion.cursor()
+
     try:
         cursor.callproc('Historiales.modHistorial',[dni,telefono,pya,gs,dniempleado])
     except cx_Oracle.IntegrityError as error:
         print('...Error modificando el historial:\n\t{}\n'.format(error))
 
 
-def getHistorial(cursor):
+def getHistorial(conexion):
+    cursor = conexion.cursor()
+
     try:
        cursor.callproc('Historiales.getHistorial',[dni])
     except cx_Oracle.IntegrityError as error:
         print('...Error obteniendo el historial:\n\t{}\n'.format(error))
     return cursor.fetchone()
 
-def addTratamiento(cursor,idtratamiento,fechai,fechaf,descripcion,dnipaciente):
+
+def addTratamiento(conexion,idtratamiento,fechai,fechaf,descripcion,dnipaciente):
+    cursor = conexion.cursor()
+    
     fechaInicio = datetime.strptime(fechai, "YYYY-MM-DD")
     fechaFinal = datetime.strptime(fechaf, "YYYY-MM-DD")
     try:
